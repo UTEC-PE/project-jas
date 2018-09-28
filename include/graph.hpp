96bf3dd7950fc06a161924fe39cfd9cc54f8ccb3
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-28 09:52:26 -0500

    Incomplete graph implementation

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,6 +1,24 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
+#include "node.hpp"
+
+template <typename T>
 class Graph{
-	
+	int nnodes;
+	Nodes<T>* nodes;
+	bool directed;
+public:
+	Graph(){};
+	Graph(int nodos, bool directed = false)
+		: nnodes(nodos), directed(directed)
+	{
+		nodes = new Nodes[nnodes];
+	}
+	void insert(T value, T neighbord, int x,int y, int z){
+		int pos;
+		for(pos = 0; i<nnodes; i++){
+
+		}
+	}
 };
 #endif
