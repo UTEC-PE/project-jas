8d2cdecb3df958fa3eea7744cde48a7e15f430ec
Arturo Cuya <arturocuya099@gmail.com>
2018-10-02 16:25:34 -0500

    Added node.hpp and a few methods for it

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "../include/node.hpp"
+#include "../include/graph.hpp"
 using namespace std;
-int main(int argc, char *argv[argc]){
-	
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
 }
