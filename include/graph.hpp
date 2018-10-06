9fc70fe62883d957673b74c4f3c9a183d6b21c02
Arturo Cuya <arturocuya099@gmail.com>
2018-10-06 00:06:42 -0500

    Back to original implementation, now with comments.

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,6 +1,42 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
-class Graph{
-	
+
+#include <vector>
+#include <list>
+#include "node.hpp"
+#include "edge.hpp"
+
+struct Traits {
+	typedef char N;
+	typedef int E;
 };
+
+template <typename Tr>
+class Graph {
+	NodeSeq nodes;
+	NodeIte ni;
+	EdgeIte ei;
+	
+public:
+	typedef Graph<Tr> self;
+
+    /* The whole graph is being passed as a trait to Node so it can have N and E
+    in it without explicitly declaring so. Same thing with Edge. */
+	typedef Node<self> node;
+	typedef Edge<self> edge;
+
+    // Vector of nodes
+	typedef std::vector<node*> NodeSeq;
+
+    /* A helper typedef for the core function.
+    This is NOT the Adjacency List. Each Node as one instead. */
+	typedef std::list<edge*> EdgeSeq;
+    
+	typedef typename Tr::N N;
+	typedef typename Tr::E E;
+	typedef typename NodeSeq::iterator NodeIte;
+	typedef typename EdgeSeq::iterator EdgeIte;
+};
+
+typedef Graph<Traits> graph;
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "graph.hpp"
 using namespace std;
-int main(int argc, char *argv[argc]){
-	
+
+main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
 }
+
